16a47d0848baa7bab702b00b59224a70384de96c
tsbarry <thiernosbarry2012@gmail.com>
2023-05-25 13:47:16 -0400

    new_update

--- sql/function.sql
+++ sql/function.sql
@@ -8,3 +8,9 @@ CREATE OR REPLACE FUNCTION market.print_hello_world() RETURNS VARCHAR AS $$
         RETURN val;
     END; $$ 
 LANGUAGE plpgsql;
+
+
+listed below are the SQL functions we should work on for this week. We'll talk more about these during class:
+Return the average and median salary of a business analyst.
+Return the company name with the highest job postings, along with respective count of job postings,
+Return the name of the job title that appears most often in your database.
