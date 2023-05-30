b431a56b425e5ebe3fe8a65fd3fc33db3c2f8982
tsbarry <thiernosbarry2012@gmail.com>
2023-05-30 17:55:15 -0400

    updates

--- sql/function.sql
+++ sql/function.sql
@@ -1,24 +1,11 @@
 
-
-CREATE OR REPLACE FUNCTION economy.print_avg_salary() RETURNS FLOAT AS $$ 
-    DECLARE
-    	val float;
-    BEGIN
-        SELECT 'avg_salary' INTO val;
-        FROM job 
-        Where 
-        RETURN val;
-    END; $$ 
-LANGUAGE plpgsql;
-
-
 CREATE OR REPLACE FUNCTION GetCompaniesWithRatingAbove(threshold NUMERIC)
 RETURNS TABLE (company_name Varchar(255), rating NUMERIC) AS $$ 
 BEGIN
   RETURN QUERY 
-  SELECT "Company Name", Rating 
-  FROM your_table_name
-  WHERE Rating > threshold;
+  SELECT "Company Name", "Rating"
+  FROM Company
+  WHERE Rating > threshold; 
 END;
 $$ LANGUAGE plpgsql;
 
@@ -27,7 +14,7 @@ RETURNS TABLE (job_id INTEGER, job_title Varchar(255), company_name TEXT) AS $$
 BEGIN
   RETURN QUERY 
   SELECT "Job_id", "Job Title", "Company Name" 
-  FROM your_table_name
+  FROM job
   WHERE Industry = industry;
 END;
 $$ LANGUAGE plpgsql;
@@ -36,9 +23,10 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION GetJobDetails(job_id INTEGER)
 RETURNS TABLE (job_title TEXT, company_name TEXT, min_salary NUMERIC, max_salary NUMERIC) AS $$
 BEGIN
-  RETURN QUERY SELECT "Job Title", "Company Name", Min_Salary, Max_Salary 
-  FROM your_table_name
-  WHERE Job_id = job_id;
+  RETURN QUERY 
+  SELECT "Job Title", "Company Name", "Min_Salary", "Max_Salary"
+  FROM job
+  WHERE Job_id = job_id; 
 END;
 $$ LANGUAGE plpgsql;
 
@@ -46,7 +34,8 @@ CREATE OR REPLACE FUNCTION GetCompaniesByState(state TEXT)
 RETURNS TABLE (company_name TEXT, city TEXT) AS $$
 BEGIN
   RETURN QUERY 
-  SELECT "Company Name", City FROM your_table_name
+  SELECT "Company Name", "City"
+  FROM state
   WHERE State = state;
 END;
 $$ LANGUAGE plpgsql;
@@ -57,6 +46,6 @@ BEGIN
   RETURN QUERY 
   SELECT AVG(Avg_Salary), MIN(Min_Salary), MAX(Max_Salary) 
   FROM your_table_name
-               WHERE City = city;
+  WHERE City = city;
 END;
 $$ LANGUAGE plpgsql;
